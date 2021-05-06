1e81ea86ced8789d85656eb443ad19e7352c1d13
unknown <damaideo6@gmail.com>
2021-05-06 22:00:31 +0700

    init project

---
+++ index.js
@@ -0,0 +1,31 @@
+const electron = require("electron");
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile("./views/index.html");
+};
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  // Windows: win32
+  // MAC: darwin
+  // Linux: linux
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
